21c90c8b8e9115e3dbae40f82fd3860c2f3cfa9f
osama.javed <osama.javed.ahmed@gmail.com>
2021-12-20 11:24:58 +0500

    Updated work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,13 @@ import { AppService } from './app.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- public _items: any;
- _showspinner: boolean;
- test: Date = new Date();
+  public _items: any;
+  _showspinner: boolean;
+  test: Date = new Date();
 
-  constructor(public _AppService: AppService,private spinner: NgxSpinnerService) {
+  constructor(public _AppService: AppService, private spinner: NgxSpinnerService) {
     this._showspinner = false;
-   }
+  }
 
   title = 'KitchenDisplayScreen';
 
@@ -25,13 +25,18 @@ export class AppComponent {
 
 
   enableButton2() {
+
+    var input = (<HTMLInputElement>document.getElementById("apikey_id")).value;;
+    alert(input);
+
     this._AppService.GetAll().subscribe((data: any) => {
-                      this._items = <any>data;
-                      debugger
-  
-                      console.log("Random Data: ", this._items);
-                  });
+      this._items = <any>data;
+      debugger
+
+      console.log("Random Data: ", this._items);
+    });
     this.syncButton = !this.syncButton;
 
+    input = '';
   }
 }
